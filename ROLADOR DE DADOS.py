20168555ba7b2608f04238eda9913b0242b41fd4
stephano <s.cristo.o@gmail.com>
2022-06-15 15:28:15 -0300

    refactor: rolador de dados

--- ROLADOR DE DADOS.py
+++ ROLADOR DE DADOS.py
@@ -12,21 +12,52 @@ class RoladorDados:
         for dado in range(1, self.n_dados + 1):
             rolagem = randint(1, self.n_lados)
             print(f'resultado do {dado}º dado: {rolagem}')
-        return soma_total
+            soma_total += rolagem
+        print(f'O resultado da soma dos dados é {soma_total}')
 
 
-try:
-    n_lados = int(input('Quantos lados tem o dado que você quer rolar? '))
-    n_dados = int(input("quantos dados você quer jogar? "))
-except ValueError:
-    print('Oops! Digite apenas números')
-else:
-    rolador = RoladorDados(n_lados, n_dados)
+def pega_n_lados():
+    n_lados = 0
+    try:
+        n_lados = int(input('Quantos lados tem o dado que você quer rolar? '))
+    except ValueError:
+        print('Oops! Digite apenas números')
+        n_lados = pega_n_lados()
+    else:
+        while n_lados <= 1:
+            print('O número de lados deve ser maior que 1!')
+            n_lados = int(input('Quantos lados tem o dado que você quer rolar? '))
+    return n_lados
+
+
+def pega_n_dados():
+    n_dados = 0
+    try:
+        n_dados = int(input('Quantos dados você quer jogar? '))
+    except ValueError:
+        print('Oops! Digite apenas números')
+        n_dados = pega_n_dados()
+    else:
+        while n_dados < 1:
+            print('O número de dados não pode ser menor que 1!')
+            n_dados = int(input('Quantos dados você quer jogar? '))
+    return n_dados
+
+
+while True:
+    numero_lados = pega_n_lados()
+    numero_dados = pega_n_dados()
+    roladorX = RoladorDados(numero_lados, numero_dados)
+
     print('Rolando o dado...')
     sleep(1)
-    resultado = rolador.rolar_dado()
+    roladorX.rolar_dado()
+    print('Fim da rolagem!')
 
-
-
-
-
+    continuar = input('Quer fazer outra rolagem? [s/n] ').strip().lower()
+    if continuar == 'n':
+        break
+    elif continuar == 's':
+        continue
+    else:
+        print('Digite somente "s" para sim e "n" para não!')
